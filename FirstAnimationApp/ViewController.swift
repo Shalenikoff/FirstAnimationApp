da670f2ffeaa11f9ee5992ddaeeb0525344e417c
Shalenikoff <KShalen@gmail.com>
2023-08-24 23:02:48 +0300

    Initial Commit

---
+++ FirstAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstAnimationApp
+//
+//  Created by Кирилл Шалеников on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
