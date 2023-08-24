73ae782e6aada4b4dc2f3b15b825e711000764b7
Shalenikoff <KShalen@gmail.com>
2023-08-24 23:32:27 +0300

    Made a view with constraints and stack of Labels. Added some settings on viewDidload method

--- FirstAnimationApp/ViewController.swift
+++ FirstAnimationApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animatedView: UIView!
+    
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+        
+        animatedView.backgroundColor = .black
+        changedLabelTextColor()
+        
+        
     }
 
 
+}
+
+
+
+extension ViewController {
+    
+    func changedLabelTextColor() {
+        presetLabel.textColor = .white
+        curveLabel.textColor = .white
+        forceLabel.textColor = .white
+        durationLabel.textColor = .white
+        delayLabel.textColor = .white
+    }
 }
 
